f3694c3454c8141c8d108e80a48cc1cbae634628
Laurent Schaffner <laurent@livestorm.co>
2022-04-17 18:22:46 -0600

    refactor: abstraction into func

--- lock-and-dispatch-event/main.go
+++ lock-and-dispatch-event/main.go
@@ -21,7 +21,27 @@ type Payload struct {
 	Method   string `json:"method"`
 }
 
+type Response struct {
+	Status string
+	Body string
+}
+
 func handleRequest(context context.Context, payload Payload) error {
+	response, err := sendRequest(context, payload)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Response body: %s\r\n", string(response.Body))
+	fmt.Printf("Response status: %s\r\n", string(response.Status))
+
+	return nil
+}
+
+func main() {
+	lambda.Start(handleRequest)
+}
+
+func sendRequest(context context.Context, payload Payload) (Response, error) {
 	printablePayload, _ := json.Marshal(payload)
 	printableContext, _ := json.Marshal(context)
 	fmt.Printf("Received context %s\r\n", string(printableContext))
@@ -45,7 +65,7 @@ func handleRequest(context context.Context, payload Payload) error {
 	// as a header to avoid 400 Bad Request
 	parsedEndpoint, err := url.Parse(endpoint)
 	if err != nil {
-		log.Fatal(err)
+		return Response{}, err
 	}
 
 	fmt.Printf("Host: %s\r\n", parsedEndpoint.Host)
@@ -59,32 +79,21 @@ func handleRequest(context context.Context, payload Payload) error {
 	}
 
 	if err != nil {
-		log.Fatal(err)
+		return Response{}, err
 	}
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Error when sending request to the server")
-		return err
+		return Response{}, err
 	}
 
 	defer resp.Body.Close()
 	responseBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal(err)
+		return Response{}, err
 	}
 
-	// TODO: deal with response and send it back to bloodbath
-	fmt.Printf("It went well for ID: %s\r\n", id)
-	fmt.Printf("Status received is: %s\r\n", resp.Status)
-	fmt.Printf("Body received is: %s\r\n", string(responseBody))
-
-	return nil
+	return Response{Status: resp.Status, Body: string(responseBody)}, nil
 }
-
-func main() {
-	lambda.Start(handleRequest)
-}
-
-// cannot unmarshal string into Go value of type lambdacontext.ClientContext
